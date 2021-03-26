1763267a4e6021f8d9fc5aa8b19502476017c04e
Eric Hay <ericdanielhay@yahoo.com>
2021-03-26 08:48:31 -0500

    Adjusted my conditional statements to include a data-

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 let description = document.getElementsByClassName("description");
 let currentDay = document.getElementById("currentDay");
 let currentDate = document.getElementById("currentDate");
-let curHour = String(moment().format('H'));
+let curHour = moment().format('H');
 let curDay = moment().format("dddd");
 let curDate = moment().format("MMM Do, YYYY");
 currentDay.innerHTML = curDay;
@@ -10,20 +10,9 @@ currentDate.innerHTML = curDate;
 let timeArray = ["t9", "t10", "t11", "t12", "t1", "t2", "t3", "t4", "t5"];
 let colorArray = ["c9", "c10", "c11", "c12", "c1", "c2", "c3", "c4", "c5"];
 
-if (curHour > 12 ) {
-    curHour = curHour - 12;
-    curHour = curHour + "PM";
-}
-else if (curHour < 12) {
-    curHour = curHour + "AM";
-}
-else{
-    curHour = curHour + "PM";
-}
-
 for (let i = 0; i < timeArray.length; i++) {
     let colorChange = document.getElementById(colorArray[i]);
-    let time = document.getElementById(timeArray[i]).innerHTML;
+    let time = document.getElementById(timeArray[i]).dataset.time;
     console.log(curHour, time, colorChange);
     if (time == curHour) {
         colorChange.classList.add("present");
