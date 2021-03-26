b869d0e41e73c0c595a14bd1960036a0c7d2fb5e
Eric Hay <ericdanielhay@yahoo.com>
2021-03-25 22:54:57 -0500

    Added formatting for time blocks.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
+let description = document.getElementsByClassName("description");
 let currentDay = document.getElementById("currentDay");
 let currentDate = document.getElementById("currentDate");
+let curHour = moment().format('h');
+console.log(curHour);
 let curDay = moment().format("dddd");
 let curDate = moment().format("MMM Do, YYYY");
 currentDay.innerHTML = curDay;
 currentDate.innerHTML = curDate;
+
