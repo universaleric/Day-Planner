7547991e2557a2bd45a82dfa4d2e380ce32441b8
Eric Hay <ericdanielhay@yahoo.com>
2021-03-26 15:06:19 -0500

    Fixed color coded time blocks.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 let description = document.getElementsByClassName("description");
 let currentDay = document.getElementById("currentDay");
 let currentDate = document.getElementById("currentDate");
-let curHour = moment().format('H');
+let curHour = moment().hour();
 let curDay = moment().format("dddd");
 let curDate = moment().format("MMM Do, YYYY");
 currentDay.innerHTML = curDay;
@@ -13,6 +13,10 @@ let colorArray = ["c9", "c10", "c11", "c12", "c1", "c2", "c3", "c4", "c5"];
 for (let i = 0; i < timeArray.length; i++) {
     let colorChange = document.getElementById(colorArray[i]);
     let time = document.getElementById(timeArray[i]).dataset.time;
+    time = parseInt(time);
+    if(time < 9) {
+        time = time + 12;
+    }
     console.log(curHour, time, colorChange);
     if (time == curHour) {
         colorChange.classList.add("present");
